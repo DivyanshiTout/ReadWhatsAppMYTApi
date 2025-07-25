9936e46d918ac8a8f839b08c04ba791d49ec1966
DivyanshiTout <153170528+DivyanshiTout@users.noreply.github.com>
2025-07-25 15:03:20 +0530

    Update testMytapi.py

--- testMytapi.py
+++ testMytapi.py
@@ -116,4 +116,5 @@ def save_message(conversation_name, message_data):
 
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Use Render's assigned port or fallback to 5000
+    app.run(host='0.0.0.0', port=port, debug=False)  
